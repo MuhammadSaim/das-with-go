45e2b4efc3b47b627659747b677b1ee392c8362e
MuhammadSaim <muhammadsaim494@gmail.com>
2024-06-03 10:21:36 +0500

    feat: add RemoveDuplicate func

--- Linkedlist/singly.go
+++ Linkedlist/singly.go
@@ -292,6 +292,46 @@ func (list *LinkedList) Middle() {
 	fmt.Printf("Middle of the list is %d\n", slow.value)
 }
 
+// remove the duplicate from a list
+func (list *LinkedList) RemoveDuplicate() {
+
+	// check there is any data in linked list
+	if list.IsEmpty() {
+		fmt.Println("Ooops! There is no data to display. please insert the data.")
+		return
+	}
+
+	// current node to store head
+	current := list.head
+
+	// initialize map for memorize the element
+	seen := make(map[int]bool)
+
+	// set value of current node to hash map
+	seen[current.value] = true
+
+	// loop through the list
+	for current.next != nil {
+
+		// if value found in hashmap means we already visited it
+		if seen[current.next.value] {
+
+			// we have to think one step ahead
+			// and if we found the value then next node will
+			// be replace with the next node
+			current.next = current.next.next
+
+		} else {
+			// register the next node value if not found and register to the map
+			seen[current.next.value] = true
+			// at the end rotate the current with the next for loop through
+			current = current.next
+		}
+
+	}
+
+}
+
 func main() {
 
 	// initialize the linkedlist
@@ -310,10 +350,19 @@ func main() {
 	linkedList.InsertAtEnd(13)
 	linkedList.InsertAtEnd(15)
 	linkedList.InsertAtEnd(11)
+	linkedList.InsertAtEnd(10)
 	linkedList.InsertAtEnd(23)
 	linkedList.InsertAtEnd(14)
 	linkedList.InsertAtEnd(78)
+	linkedList.InsertAtEnd(11)
 	linkedList.InsertAtEnd(90)
+	linkedList.InsertAtEnd(14)
+
+	// display the list
+	linkedList.Display()
+
+	// remove duplicates
+	linkedList.RemoveDuplicate()
 
 	// display the list
 	linkedList.Display()
